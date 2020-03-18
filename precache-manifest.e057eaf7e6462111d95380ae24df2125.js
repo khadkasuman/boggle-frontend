ab29bfd2ea41a8146c9d40e0dfed96eb93063d73
khadkasuman <khadkasuman20@outlook.com>
2020-03-18 08:22:12 +0545

    gh-pages deployment

---
+++ precache-manifest.e057eaf7e6462111d95380ae24df2125.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be3157fd5308f9fe65195716bb0428b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6951ea3be9faeac0bcb1",
+    "url": "/static/js/2.c57c3c68.chunk.js"
+  },
+  {
+    "revision": "d398182e4cd8b7fa7825",
+    "url": "/static/js/main.ad8ebd38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
